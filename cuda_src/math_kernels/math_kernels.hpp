90b6ca83fc7b54a94e81e4260aab9d2a372e637d
Shuyang Sun <sunbuffett@gmail.com>
2017-05-15 23:26:00 -0400

    Added some functions in math_kernels.

--- cuda_src/math_kernels/math_kernels.hpp
+++ cuda_src/math_kernels/math_kernels.hpp
@@ -10,5 +10,22 @@
 #ifndef OPPC_CUDA_SRC_MATH_KERNELS_HPP_
 #define OPPC_CUDA_SRC_MATH_KERNELS_HPP_
 
+__global__ void FPO_1();
+__global__ void FPO_2();
+__global__ void FPO_3();
+__global__ void FPO_4();
+__global__ void FPO_5();
+__global__ void FPO_6();
+__global__ void FPO_7();
+__global__ void FPO_8();
+__global__ void FPO_9();
+__global__ void FPO_10();
+__global__ void FPO_11();
+__global__ void FPO_12();
+__global__ void FPO_13();
+__global__ void FPO_14();
+__global__ void FPO_15();
+__global__ void FPO_16();
+
 #endif
 
